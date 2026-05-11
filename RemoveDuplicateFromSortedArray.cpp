a99116c53131442cc9f6f5684e8030d4a5b2f60a
adtshrm007 <adtshrm1@hmail.com>
2026-05-11 22:47:05 +0530

    Solved 1 DSA question

---
+++ RemoveDuplicateFromSortedArray.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+
+        for(int j = 1; j < nums.size(); j++) {
+            if(nums[i] != nums[j]) {
+                i++;
+                swap(nums[i], nums[j]);
+            }
+        }
+
+        return i + 1;
+    }
+};
+
+int main() {
+
+    Solution s;
+
+    vector<int> nums = {1,1,2};
+
+    int ans = s.removeDuplicates(nums);
+
+    cout << ans << endl;
+
+    return 0;
+}
