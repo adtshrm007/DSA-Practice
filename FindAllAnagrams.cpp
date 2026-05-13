7027c66f5b0e5cdeeefba1e6f545c10889e320b0
adtshrm007 <adtshrm1@gmail.com>
2026-05-13 11:38:04 +0530

    Solved 1 DSA question

--- FindAllAnagrams.cpp
+++ FindAllAnagrams.cpp
@@ -45,3 +45,45 @@ public:
         return ans;
     }
 };
+
+
+
+//2nd Approach
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+
+        if(s.size() < p.size()) return ans;
+
+        vector<int> pFreq(26, 0);
+        vector<int> window(26, 0);
+
+        for(char ch : p) {
+            pFreq[ch - 'a']++;
+        }
+
+        int k = p.size();
+
+        for(int i = 0; i < k; i++) {
+            window[s[i] - 'a']++;
+        }
+
+        if(window == pFreq) {
+            ans.push_back(0);
+        }
+
+        for(int i = k; i < s.size(); i++) {
+
+            window[s[i] - 'a']++;
+
+            window[s[i-k] - 'a']--;
+
+            if(window == pFreq) {
+                ans.push_back(i-k+1);
+            }
+        }
+
+        return ans;
+    }
+};
