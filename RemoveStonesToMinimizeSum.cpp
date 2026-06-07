de51a1fb65ae20385e64946a374ce73609e2a0db
adtshrm007 <adtshrm1@gmail.com>
2026-06-07 15:59:49 +0530

    Solved 1 DSA question

---
+++ RemoveStonesToMinimizeSum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        priority_queue<int> pq;
+
+        for (int p : piles)
+            pq.push(p);
+
+        while (k--) {
+            int x = pq.top();
+            pq.pop();
+
+            pq.push((x + 1) / 2);
+        }
+
+        int ans = 0;
+
+        while (!pq.empty()) {
+            ans += pq.top();
+            pq.pop();
+        }
+
+        return ans;
+    }
+};
