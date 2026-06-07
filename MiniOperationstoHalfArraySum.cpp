8dd81ad7f9f18a4189502025ba01a1dd28fb5bac
adtshrm007 <adtshrm1@gmail.com>
2026-06-07 15:59:35 +0530

    Solved 1 DSA question

---
+++ MiniOperationstoHalfArraySum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int halveArray(vector<int>& nums) {
+        priority_queue<double> pq;
+        double sum = 0;
+
+        for (int x : nums) {
+            sum += x;
+            pq.push(x);
+        }
+
+        double target = sum / 2.0;
+        double reduced = 0;
+        int operations = 0;
+
+        while (reduced < target) {
+            double x = pq.top();
+            pq.pop();
+
+            double half = x / 2.0;
+            reduced += half;
+
+            pq.push(half);
+            operations++;
+        }
+
+        return operations;
+    }
+};
