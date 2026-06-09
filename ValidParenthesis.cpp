113d318d9b86816db9cc0c248919783572791377
adtshrm007 <adtshrm1@gmail.com>
2026-06-10 00:10:36 +0530

    Solved 1 DSA question

---
+++ ValidParenthesis.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(st.size()==0||s[i]=='('||s[i]=='{'||s[i]=='['){
+                st.push(s[i]);
+            }
+            else{
+                if(s[i]==')'){
+                    if(st.top()=='('){
+                        st.pop();
+                    }
+                    else{
+                        st.push(s[i]);
+                    }
+                }
+                else if(s[i]=='}'){
+                    if(st.top()=='{'){
+                        st.pop();
+                    }
+                    else{
+                        st.push(s[i]);
+                    }
+                }
+                else{
+                    if(st.top()=='['){
+                        st.pop();
+                    }
+                    else{
+                        st.push(s[i]);
+                    }
+                }
+            }
+        }
+        if(st.size()==0){
+            return true;
+        }
+        return false;
+    }
+};
